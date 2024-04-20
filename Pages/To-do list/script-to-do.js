c05e2be72fca4d97d35e8453dff176e0d7498808
shashwatpathak24 <163170482+shashwatpathak24@users.noreply.github.com>
2024-04-20 23:49:58 +0530

    Add files via upload

---
+++ Pages/To-do list/script-to-do.js
@@ -0,0 +1,40 @@
+const inputBox=document.getElementById('input-box');
+const listContainer=document.getElementById('list-container');
+
+
+function addTask(){
+    if(inputBox.value ===''){
+        alert("You must write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span= document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(control){
+    if(control.target.tagName === "LI"){
+        control.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(control.target.tagName==="SPAN"){
+        control.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
